bc41e5f4f6e91c95ef36ec2db42b9ac2e6b3f47b
Jullev <setiyawanjullev@gmail.com>
2017-03-01 15:37:49 +0700

    Based Commit Awal

---
+++ app/src/main/java/com/example/setiyawan/mobileapps/Activity_utama.java
@@ -0,0 +1,20 @@
+package com.example.setiyawan.mobileapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity_utama extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+
+    }
+}
---
+++ app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
@@ -0,0 +1,16 @@
+package com.example.setiyawan.mobileapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by setiyawan on 3/1/17.
+ */
+
+public class Layout2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+    }
+}
