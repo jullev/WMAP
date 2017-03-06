b7571fa0a358d41e7269521c025066cb4b3f187a
DESKTOP-K6OO5R5\Windows-10 <venatafatmala@gmail.com>
2017-03-07 01:06:52 +0700

    comit

--- app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
+++ app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
@@ -12,6 +12,6 @@ public class Layout2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout2);
-        //perubahan 
+        //perubahan
     }
 }
