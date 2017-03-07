1cfa0c3d089e0ccdba34d862cb9d563732cdbf68
Jullev <setiyawanjullev@gmail.com>
2017-03-07 13:02:16 +0700

    commit perubahan

--- app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
+++ app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
@@ -2,16 +2,22 @@ package com.example.setiyawan.mobileapps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by setiyawan on 3/1/17.
  */
 
 public class Layout2 extends Activity {
+    Button btn;
+    EditText edTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout2);
-        //perubahan 
+        //perubahan
+        btn = (Button) findViewById(R.id.button);
+        edTest = (EditText) findViewById(R.id.textView2);
     }
 }
