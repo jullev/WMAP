8b5a0af2a908e594a062510a449a9f47f7174e97
Jullev <setiyawanjullev@gmail.com>
2017-03-08 15:32:43 +0700

    Cache & Bundle

--- app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
+++ app/src/main/java/com/example/setiyawan/mobileapps/Layout2.java
@@ -2,8 +2,10 @@ package com.example.setiyawan.mobileapps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by setiyawan on 3/1/17.
@@ -16,8 +18,12 @@ public class Layout2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout2);
+
+        String shared = getSharedPreferences("CacheApp",MODE_PRIVATE).getString("pref","a").toString();
+        Toast.makeText(Layout2.this,"Share : "+shared,Toast.LENGTH_LONG).show();
         //perubahan
         btn = (Button) findViewById(R.id.button);
-        edTest = (EditText) findViewById(R.id.textView2);
+        edTest = (EditText) findViewById(R.id.editText2);
+        edTest.setText(shared);
     }
 }
